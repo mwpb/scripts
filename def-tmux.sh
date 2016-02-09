ce3cc4f512af4fec9b7f79582d36214ca1a6a584
Matthew Burke <matthew.burke@cantab.net>
2016-02-09 10:57:53 +0000

    updated def-tmux to start google calendar

--- def-tmux.sh
+++ def-tmux.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
 
-tmux new-session -d 'python ~/scripts/ranger/ranger.py'
-tmux split-window -h 'python ~/scripts/ranger/ranger.py'
-tmux new-window 'calcurse'
+tmux new-session -d 'python ~/scripts-borrowed/ranger/ranger.py'
+tmux split-window -h 'python ~/scripts-borrowed/ranger/ranger.py'
+tmux new-window 'vim -c Calendar'
 tmux split-window -h 'mutt'
 
 tmux -2 attach-session -d
