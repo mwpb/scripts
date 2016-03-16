8c11d05ec48f1a0b39f10624d2d4b3ed48c1a570
Matthew Burke <matthew.burke@cantab.net>
2016-03-16 10:55:14 +0000

    refactored vimwiki2markdown

--- vimwiki2markdown2html/markdown_folder2html.py
+++ vimwiki2markdown2html/markdown_folder2html.py
@@ -1,7 +1,36 @@
 from sys import argv
 import markdown, os, errno, codecs
-from vimwiki2markdown2html_functions import markdown_folder2html
 
-script, dirname = argv
+html_head = '''
+<head>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+'''
+def markdown_folder2html(dirname):
+    target_dir = dirname+"html/"
+    try:
+        os.makedirs(target_dir)
+    except OSError:
+        pass
+    
+    print "\nTarget directory is: %s" % target_dir
+    print "Converting files to html...\n"
+    
+    for file_handle in os.listdir(dirname):
+        print file_handle
+        filename = os.path.splitext(file_handle)[0]
+        if not os.path.isdir(dirname+'/'+filename):
+            if not file_handle.startswith('.'):
+                input_file = codecs.open(dirname+"/"+file_handle,'r',encoding="utf-8")
+                md = input_file.read()
+                f_out = codecs.open(dirname+'html/'+filename+'.html','w',encoding="utf-8",errors="xmlcharrefreplace")
+                output = markdown.markdown(md)
+                print output
+                f_out.write(html_head)
+                f_out.write(output)
+                input_file.close()
+                f_out.close()
 
-markdown_folder2html(dirname)
+if __name__ =='__main__':
+    script, dirname = argv
+    markdown_folder2html(dirname)
--- vimwiki2markdown2html/vimwiki_folder2markdown.py
+++ vimwiki2markdown2html/vimwiki_folder2markdown.py
@@ -1,8 +1,24 @@
-import os
-from vimwiki2markdown2html import vimwiki_folder
-
+import re, os, errno, codecs, markdown
+from vimwiki2markdown import *
 from sys import argv
 
-script, dirname = argv
+def vimwiki_folder2markdown(dirname):
+    target_dir = dirname+"/markdown"
+    try:
+        os.makedirs(target_dir)
+    except OSError:
+        pass
+    
+    print "\nTarget directory is: %s" % target_dir
+    print "Converting files to markdown...\n"
+    
+    for filename in os.listdir(dirname):
+        print filename
+        ext = os.path.splitext(filename)[1]
+        if ext == ".md":
+            print filename
+            vimwiki2markdown(dirname,filename)
 
-vimwiki_folder2markdown(dirname)
+if __name__ == '__main__':
+    script, dirname = argv
+    vimwiki_folder2markdown(dirname)
