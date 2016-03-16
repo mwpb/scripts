b4e6cd8403caecfe2c0af947619b504bc27d377d
Matthew Burke <matthew.burke@cantab.net>
2016-03-16 10:55:25 +0000

    refactored vimwiki to markdown

---
+++ vimwiki2markdown2html/vimwiki2markdown.py
@@ -0,0 +1,24 @@
+import re, os, errno, codecs, markdown
+from sys import argv
+
+def vimwiki2markdown(dirname,filename):
+    f = open(dirname+"/"+filename)
+    # Remove output file if already exists
+    try:
+        os.remove(dirname+"/markdown/"+filename)
+    except OSError:
+        pass
+    text = f.read()
+    f_out = open(dirname+"/markdown/"+filename,"a+")
+    
+    changed_text = re.sub('\[\[(.+?)\]\]',r'[\1](\1)',text)
+    print changed_text
+    f_out.write(changed_text)
+    
+    f.close()
+    f_out.close()
+    print filename
+
+if __name__ == '__main__':
+    script, dirname, filename = argv
+    vimwiki2markdown(dirname,filename)
