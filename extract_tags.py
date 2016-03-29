c2117fcb56832134ff5caec590026d4c083b0cd7
Matthew Burke <matthew.burke@cantab.net>
2016-03-29 10:55:50 +0100

    changed extract tags

--- extract_tags.py
+++ extract_tags.py
@@ -12,7 +12,12 @@ def extract_tags(dir_path):
                 for line in file_object:
                     tag = re.search('(.*)@(.+)@',line)
                     if tag:
-                        tag_dict[tag.group(2)] = tag.group(1)
+                        new_tag = [tag.group(1)]
+                        if tag.group(2) in tag_dict:
+                            tag_dict[tag.group(2)] = tag_dict[tag.group(2)]+new_tag
+                        else:
+                            tag_dict[tag.group(2)] = new_tag
+                file_object.close()
             except:
                 pass
     return tag_dict
@@ -21,9 +26,11 @@ def print_tags_to_file(dir_path):
     file_object = open(dir_path+'next.txt','w')
     file_object.write('# Next\n\n')
     tag_dict = extract_tags(dir_path)
-    tag_dict['--**'+date.today().__str__()+'**'] = ''
+    tag_dict[date.today().__str__()+'-----------'] = '-'
     for key in sorted(tag_dict):
-        file_object.write('{:18} {} \n'.format(key,tag_dict[key])+'\n')
+        for value in tag_dict[key]:
+            file_object.write('{:18} {} \n'.format(key,value)+'\n')
+    file_object.close()
 
 if __name__ == '__main__':
     script, dir_path = argv
